6d2767e2747ea7439b9d217e835235213d59dc96
eduardo aleixo <eduardoaleixomartins@gmail.com>
2022-03-20 17:54:25 -0300

    wip initial pilula

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,31 +1,23 @@
-import { Composition } from 'remotion';
-import { AudiogramComposition } from './Composition';
-import './fonts.css';
-import { PromoComposition } from './PromoComposition';
-import './style.css';
+import { Composition } from "remotion";
+import { AudiogramComposition } from "./Composition";
+import "./fonts.css";
+import "./style.css";
 
 const fps = 30;
-const durationInFrames = 30 * fps;
+//const durationInFrames = 30 * fps;
+const durationInFrames = 18 * fps;
 
 export const RemotionVideo: React.FC = () => {
-	return (
-		<>
-			<Composition
-				id="Audiogram"
-				component={AudiogramComposition}
-				durationInFrames={durationInFrames}
-				fps={fps}
-				width={1080}
-				height={1080}
-			/>
-			<Composition
-				id="Promo"
-				component={PromoComposition}
-				durationInFrames={durationInFrames}
-				fps={fps}
-				width={1280}
-				height={720}
-			/>
-		</>
-	);
+  return (
+    <>
+      <Composition
+        id="Audiogram"
+        component={AudiogramComposition}
+        durationInFrames={durationInFrames}
+        fps={fps}
+        width={1080}
+        height={1080}
+      />
+    </>
+  );
 };
